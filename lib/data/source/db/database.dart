4f2ba2634ed58cc3b9a0a9e807182467a83ba6ea
Archit Kumar <architxkumar@proton.me>
2025-12-31 21:42:56 +0530

    feat: add list database model

--- lib/data/source/db/database.dart
+++ lib/data/source/db/database.dart
@@ -12,13 +12,41 @@ class TodoItems extends Table {
     const Constant(false),
   )();
   late final Column<DateTime> dueDate = dateTime().nullable()();
+
   // NOTE: Since the values are being set in the service, we don't set
   // default values here.
   late final Column<DateTime> createdAt = dateTime()();
   late final Column<DateTime> updatedAt = dateTime()();
+
+  // NOTE: Default value is supplied to deal with migration
   late final Column<int> sortOrder = integer().withDefault(
     const Constant(0),
   )();
+}
+
+class ListItems extends Table {
+  late final Column<int> id = integer().autoIncrement()();
+  late final Column<String> title = text().unique().withLength(min: 1)();
+
+  // NOTE: Default value isn't needed as it's a fresh instance rather than migration
+  late final Column<int> sortOrder = integer().customConstraint(
+    'NOT NULL UNIQUE CHECK(sortOrder >= 0)',
+  )();
+  late final Column<bool> isDefault = boolean().withDefault(
+    const Constant(false),
+  )();
+
+  // NOTE: The service will set these values
+  late final Column<DateTime> createdAt = dateTime()();
+  late final Column<DateTime> updatedAt = dateTime()();
+
+  /// Unicode value of the emoji
+  /// E.g. U+1F600 for 😀
+  late final Column<String> emoji = text().nullable()();
+
+  /// The hex value of the color
+  /// E.g. #FF5733
+  late final Column<String> color = text().nullable()();
 }
 
 @DriftDatabase(tables: [TodoItems])
